ed6d9935654e19118b807fead5686606b4fda228
youjaepark <youjaepark@gmail.com>
2023-03-22 11:58:54 +0900

    03/22

---
+++ 0322 Test.py
@@ -0,0 +1,12 @@
+a = (1,2,3)
+print(a)
+b = {"name": "Youjaep","age":18}
+print(b["name"])
+b = {"name": "Youjaep",
+     "age" : 18,
+     "subject" : "science"   
+     }
+print(list(b.keys()))
+print(list(b.values()))
+print(b.items())
+print(b.get("Name"))
