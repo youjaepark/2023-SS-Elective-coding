d7f049bde03a4645253cc3c103c2ad8a8032dd17
youjaepark <youjaepark@gmail.com>
2023-03-29 12:02:05 +0900

    03/29

--- 0322 Test.py
+++ 0322 Test.py
@@ -12,3 +12,20 @@ num = pin[7:]
 print(yyyymmdd)
 print(num)
 print(pin[7])
+
+a = "a:b:c:d"
+b = a.replace(':','#')
+print(b)
+
+a = [1,3,5,4,2]
+a.sort()
+a.reverse()
+print(a)
+
+a = ["life","is","too","short"]
+result = ' '.join(a)
+print(result)
+ 
+a = (1,2,3)
+a += (4,)
+print(a)
