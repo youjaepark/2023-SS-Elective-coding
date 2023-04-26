3f1f7412bad343bec032d6d49ac1b2dedcaec337
youjaepark <102572446+youjaepark@users.noreply.github.com>
2023-04-26 11:42:54 +0900

    .

--- 0419.py
+++ 0419.py
@@ -1,13 +1,5 @@
-while True:
-    money = int(input("돈을 입력하세요: "))
-    if money >= 4500:
-        print("컵밥")
-        print(f"잔돈은 {money-4500} 원 입니다.")
-    elif money >= 2800:
-        print("초콜렛")
-        print(f"잔돈은 {money-2800} 원 입니다.")
-    elif money >= 1500:
-        print("제로 콜라")
-        print(f"잔돈은 {money-1500} 원 입니다.")          
-    else: 
-        print("돈이 부족합니다")
+a = 0
+while a <100:
+    if a%3 == 0:
+        print(a)
+    a +=1
