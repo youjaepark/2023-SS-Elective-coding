dadeb240d784128edeb20687d5e78bb5a4e694de
youjaepark <102572446+youjaepark@users.noreply.github.com>
2023-04-26 10:50:37 +0900

    .

--- 0419.py
+++ 0419.py
@@ -1,10 +1,11 @@
-pocket = ["돈","카드","핸드폰"]
-if "돈" in pocket:
-    print("택시")
-elif "카드" in pocket:
-    print("택시")
-elif "전화기" in pocket:
-    print("택시")
-else:
-    print("택시")
-
+coffee = 10
+while coffee >0:
+    money = int(input("돈을 입력하세요: "))
+    if money >= 300:
+        print("커피 나옵니다.")
+        coffee -= 1 
+        print(f"남음 커피 재고량은 {coffee} 입니다.")
+        print(f"잔돈은 {money-300} 입니다.")
+    else: 
+        print("돈이 부족합니다")
+print("커피가 다 떨어졌습니다.")
