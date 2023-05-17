3a15b44135dfd5f7acb1263fc727ea27564164f3
youjaepark <qkrdbwo0506@gmail.com>
2023-05-17 11:41:37 +0900

    .

--- 0419.py
+++ 0419.py
@@ -1,28 +1,29 @@
-# prompt = """
-# 1. ADD
-# 2. DEL
-# 3. UPDATE
-# 4. EXIT
-# """
-# number = 0
-# while number != 4:
-#     print(prompt)
-#     number = int(input("ENTER NUMBER: "))
-#     if number == 1:
-#         print("데이터를 추가합니다.")
-#     elif number == 2:
-#         print("데이터를 삭제합니다.")
-#     elif number == 3:
-#         print("데이터를 수정합니다.")
-#     elif number == 4:
-#         print("프로그램에서 나갑니다.")
+"""
+a = "Life is too short, you need python."
 
-coffee = 10
-money = 5000
-while (coffee >= 0 and money >= 100):
-    print("돈을 받았으니 커피를 드리겠습니다.")
-    print(f"남은 커피는 {coffee} 입니다. 잔액은 {money} 입니다")
-    if coffee == 0:
-        print("커피가 떨어졌습니다")
-    coffee -= 1
-    money -= 100
+if "wife" in a:
+    print("print")
+elif "python" in a and "you" in a:
+    print("python")
+elif "shirt" in a:
+    print("shirt")
+elif "need" in a:
+    print("need")
+else:
+    print("none")
+
+count = 1
+sum = 0
+while count <= 1000:
+    if count % 3 ==0:
+        sum += count
+    count+=1
+print(sum)
+
+row = 5
+while row >= 1:
+    print("*" * row)
+    row -= 1
+"""
+for i in range(1, 100):
+    print(i)
