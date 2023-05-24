582e13fbb4fafb853fa911e8a7db0d350fa01dda
youjaepark <qkrdbwo0506@gmail.com>
2023-05-24 12:15:57 +0900

    .

--- 0419.py
+++ 0419.py
@@ -32,9 +32,6 @@ if num1 > num2:
 else:
     print("No")
     
-"""
-
-
 def cal():
     a = int(input("첫번째 숫자를 입력하세요: "))
     b = int(input("두번째 숫자를 입력하세요: "))
@@ -52,3 +49,23 @@ def cal():
 
 
 print(cal())
+
+def three(a):
+    if a % 3 == 0:
+        print("3의 배수 입니다.")
+    else:
+        print("3의 배수가 아닙니다.")
+ 
+i = int(input("숫자를 입력하세요: "))
+three(i)
+"""
+
+h = int(input("키를 입력하세요: "))
+w = int(input("몸무게를 입력하세요: "))
+
+
+def bmi():
+    print(round(int(w) / (int(h) / 100) ** 2, 2))
+
+
+bmi()
