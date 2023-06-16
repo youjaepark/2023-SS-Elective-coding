2b501d0c0f3df2a8ee2334d952db6cae0edca994
youjaepark <102572446+youjaepark@users.noreply.github.com>
2023-06-17 02:57:25 +0900

    기말고사 프로젝트 최종

--- 기말고사 프로젝트.py
+++ 기말고사 프로젝트.py
@@ -1,10 +1,8 @@
-# data.txt 파일 생성
 import random
 
 first_name_samples = "김이박최정강조윤장임"
 middle_name_samples = "민서예지도하주윤채현지"
 last_name_samples = "준윤우원호후서연아은진"
-
 
 def random_name():
     result = ""
@@ -13,34 +11,54 @@ def random_name():
     result += random.choice(last_name_samples)
     return result
 
-
-with open("C:/Users/haven06/Desktop/파이썬 고등/2023-SS-Elective-coding/data.txt", "w") as f:
+# data.txt 파일 생성
+with open("data.txt", "w") as d:
     for i in range(1, 101):
-        f.write(
+        d.write(
             random_name()
-            + ", "
+            + ","
             + str(random.randrange(30, 100))
-            + ", "
+            + ","
             + str(random.randrange(100, 190))
             + "\n"
         )
 
 # 텍스트파일 복제
-with open("C:/Users/haven06/Desktop/파이썬 고등/2023-SS-Elective-coding/data.txt", "r") as f:
-    with open(
-        "C:/Users/haven06/Desktop/파이썬 고등/2023-SS-Elective-coding/bmi.txt", "w"
-    ) as b:
-        data = f.read()
+with open("data.txt", "r") as d:
+    data = d.read()
+    with open("bmi.txt", "w") as b:
         b.write(data)
 
-with open("C:/Users/haven06/Desktop/파이썬 고등/2023-SS-Elective-coding/bmi.txt", "r") as b:
-    while True:
-        line = b.readline()
-        word = line.split(",")
-        print(word)
-        weight = int(word[1])
-        height = int(word[2][1:4])
-        bmi = height / (weight**2)
-        print(bmi)
-        if not line:
-            break
+#BMI 계산
+def calculate_bmi(weight, height_cm):
+    height_m = height_cm / 100
+    bmi = weight / (height_m ** 2) 
+    return bmi
+
+#BMI를 바탕으로 체형 판단
+def get_weight_status(bmi):
+    if bmi < 18.5:
+        return "저체중"
+    elif 18.5 <= bmi < 25:
+        return "정상"
+    elif 25 <= bmi < 30:
+        return "과체중"
+    else:
+        return "비만"
+    
+#bmi.txt 파일 읽어들이기
+with open("bmi.txt", 'r') as file:
+    lines = file.readlines()
+updated_lines = []
+
+for line in lines:
+    name, weight, height = line.strip().split(',')
+    height = int(height)
+    weight = int(weight)
+    bmi = calculate_bmi(weight, height)
+    weight_status= get_weight_status(bmi)
+    updated_line = f"{name},{weight},{height},{round(bmi)},{weight_status}\n"
+    updated_lines.append(updated_line)
+
+with open("bmi.txt", 'w') as file:
+    file.writelines(updated_lines)
